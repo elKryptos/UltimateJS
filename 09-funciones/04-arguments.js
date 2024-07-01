b5581ce249857c79e4bf55a88ab5998312fb7a30
elKryptos <hans_09@hotmail.it>
2024-07-01 10:59:46 +0200

    fat arrow

--- 09-funciones/04-arguments.js
+++ 09-funciones/04-arguments.js
@@ -1,7 +1,15 @@
 function suma(a, b) {
    // return a + b
-   return Array.from(arguments).reduce((acc, el) => acc + el);
+   return Array.from(arguments)
+      .reduce((acc, el) => acc + el);
 }
+/** Arguments es muy similar a un array per no lo es, es un Objeto
+ * que esta disponible solo al interno de funciones tradicionales y non 
+ * arrow functions () => {}
+ * Argumentos dinamico, permite de trabajar con funciones que puede
+ * aceptar un numero indefinido de argumentos, permitiendo gestionar 
+ * funciones varias 
+ */
 
-   console.log(suma(5,2, 3, 4, 5, 6));
-// console.log(suma(2))
+console.log(suma(2, 6));
+console.log(suma(5,2, 3, 4, 5, 6));
---
+++ 09-funciones/05-fat-arrow.js
@@ -0,0 +1,15 @@
+const suma = (a, b) => {
+    return Array.from(arguments)
+        .reduce((acc, el) => acc + el)
+}
+
+/**Las fat arrow function non tienen arguments por lo que en la 
+ * console me aparece error, una fat arrow function es como una function
+ * pero reducida y es usada generalmente para hacer calculos.
+ * Una fat arrow function no tiene el propio binding 'this' ma viene
+ * eredado del contexto lexical en el que viene definidas.
+ * 
+ * Una fat arrow function tampoco tienen un super();
+ **/
+
+console.log(suma(1,23,3,4,5))
