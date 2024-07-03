e5ce0cec1a4ffccb01fa304d93f8decb14226f5a
elKryptos <154229030+elKryptos@users.noreply.github.com>
2024-07-03 23:20:59 +0200

    added file this.js

---
+++ 09-funciones/11-this.js
@@ -0,0 +1,36 @@
+/**
+ * Dentro de un objeto this hace referencia a un objeto
+ * En una funcion this hace referencia al objeto window, global
+ * Si se usa new have referencia al objeto que serà creado
+ */
+
+const user = {
+    name: 'Nicolas', 
+    login() {
+        console.log(this);
+    }
+};
+
+user.logout = function() {
+    console.log(this)
+}
+
+user.logout();
+
+function log()) {
+    console.log(this)
+}
+
+log();
+
+
+function Log(mensaje) {
+    this.mensaje = mensaje;
+    console.log(this);
+}
+
+// al usar new se crea un objeto literal
+// se vincula este objeto a this
+// se vincula el prototipo
+// si no retorna nada entonces retorna this
+const l = new Log('Hola mundo');
